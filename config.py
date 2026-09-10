1121ce847720d028918954be3fd3d319f7e11f02
manish182003 <manishjoshi182003@gmail.com>
2026-09-10 11:01:04 +0530

    chore(config): set production daily schedule to 17:00 (5:00 PM IST) daily

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ NOTION_API_KEY = os.getenv("NOTION_API_KEY", "")
 NOTION_DATABASE_ID = os.getenv("NOTION_DATABASE_ID", "")
 
 # Scheduler config
-RUN_HOUR = int(os.getenv("RUN_HOUR", "8"))
+RUN_HOUR = int(os.getenv("RUN_HOUR", "17"))
 RUN_MINUTE = int(os.getenv("RUN_MINUTE", "0"))
 TIMEZONE = os.getenv("TIMEZONE", "Asia/Kolkata")
 
